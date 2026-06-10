22ac4123d8c8e9a551d56f59a3838bf135af4ace
Ivan Alekseev <ivan.alieksieiev1@gmail.com>
2026-06-10 10:16:29 +0300

    Project init, setting up the db

---
+++ packages/server/index.ts
@@ -0,0 +1,12 @@
+import express, { Request, Response } from "express";
+
+const PORT = 3000;
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
