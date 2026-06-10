80784a09b829d49e004713438796c502f5106ff8
Ivan Alekseev <ivan.alieksieiev1@gmail.com>
2026-06-10 11:06:59 +0300

    Adding simple migrations, refine the DB schema

---
+++ packages/server/prisma/migrations/20260610074811_init/migration.sql
@@ -0,0 +1,35 @@
+CREATE TABLE "Product" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "price" DOUBLE PRECISION NOT NULL,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+CREATE TABLE "Review" (
+    "id" SERIAL NOT NULL,
+    "author" TEXT NOT NULL,
+    "rating" INTEGER NOT NULL,
+    "content" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "productId" INTEGER NOT NULL,
+
+    CONSTRAINT "Review_pkey" PRIMARY KEY ("id")
+);
+
+CREATE TABLE "Summary" (
+    "id" SERIAL NOT NULL,
+    "productId" INTEGER NOT NULL,
+    "content" TEXT NOT NULL,
+    "generatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "expiresAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Summary_pkey" PRIMARY KEY ("id")
+);
+
+CREATE UNIQUE INDEX "Summary_productId_key" ON "Summary"("productId");
+
+ALTER TABLE "Review" ADD CONSTRAINT "Review_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+ALTER TABLE "Summary" ADD CONSTRAINT "Summary_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
