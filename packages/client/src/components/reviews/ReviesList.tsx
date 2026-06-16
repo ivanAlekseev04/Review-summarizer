c2ef4217d744fe532152540276ef21b620c292b9
Ivan Alekseev <ivan.alieksieiev1@gmail.com>
2026-06-16 23:18:26 +0300

    Add loading skeletons with basic error handling

--- packages/client/src/components/reviews/ReviesList.tsx
+++ packages/client/src/components/reviews/ReviesList.tsx
@@ -1,5 +1,7 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import { StarRating } from './StarRating';
+import Skeleton from 'react-loading-skeleton';
 
 type Props = {
     productId: number;
@@ -20,25 +22,57 @@ type ReviewsResponse = {
 
 const ReviesList = ({ productId }: Props) => {
     const [reviewDate, setReviewData] = useState<ReviewsResponse>();
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState('');
 
     const fetchReviews = async () => {
-        const { data } = await axios.get<ReviewsResponse>(
-            `/api/products/${productId}/reviews`
-        );
-        setReviewData(data);
+        try {
+            setIsLoading(true);
+
+            const { data } = await axios.get<ReviewsResponse>(
+                `/api/products/${productId}/reviews`
+            );
+
+            setReviewData(data);
+        } catch (error) {
+            console.error(error);
+            setError('Could not fetch the reviews. Try again!');
+        } finally {
+            setIsLoading(false);
+        }
     };
 
     useEffect(() => {
         fetchReviews();
     }, []);
 
+    if (isLoading) {
+        return (
+            <div className="flex flex-col gap-5">
+                {[1, 2, 3].map((placeholder) => (
+                    <div key={placeholder}>
+                        <Skeleton width={150} />
+                        <Skeleton width={100} />
+                        <Skeleton count={2} />
+                    </div>
+                ))}
+            </div>
+        );
+    }
+
+    if (error) {
+        return <p className='text-red-500'>{error}</p>
+    }
+
     return (
-        <div className='flex flex-col gap-5'>
-            {reviewDate?.reviews.map(review => (
+        <div className="flex flex-col gap-5">
+            {reviewDate?.reviews.map((review) => (
                 <div key={review.id}>
-                    <div className='font-semibold'>{review.author}</div>
-                    <div>Rating: {review.rating}/5</div>
-                    <div className='py-2'>{review.content}</div>
+                    <div className="font-semibold">{review.author}</div>
+                    <div>
+                        <StarRating value={review.rating}></StarRating>
+                    </div>
+                    <div className="py-2">{review.content}</div>
                 </div>
             ))}
         </div>
---
+++ packages/client/src/components/reviews/StarRating.tsx
@@ -0,0 +1,17 @@
+import { FaStar } from 'react-icons/fa';
+
+type Props = {
+    value: number; // rating from 0 to 5
+};
+
+export const StarRating = ({ value }: Props) => {
+    const placeholders = [1, 2, 3, 4, 5];
+
+    return (
+        <div className='flex gap-1'>
+            {placeholders.map(p => (
+                <FaStar key={p} className={p <= value ? 'text-yellow-400' : 'text-gray-300'} />
+            ))}
+        </div>
+    );
+};
